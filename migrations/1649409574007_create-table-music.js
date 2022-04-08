8e63dfea2eeba99bc14a5bd22bceebd12a30925b
Naufal Muhammad Farras <nmfarras@gmail.com>
2022-04-08 16:32:30 +0700

    add small database edit

--- migrations/1649409574007_create-table-music.js
+++ migrations/1649409574007_create-table-music.js
@@ -31,11 +31,11 @@ exports.up = (pgm) => {
       type: 'INTEGER',
       notNull: true,
     },
-    genre: {
+    performer: {
       type: 'TEXT',
       notNull: true,
     },
-    performer: {
+    genre: {
       type: 'TEXT',
       notNull: true,
     },
